55ee4931b6a47618d7308c4a08b9f57ab6aec5e9
stevieflyer <steveflyer7@gmail.com>
2023-08-22 14:55:00 +0800

    add selectors

--- youcreep/page_parser/selectors/common.py
+++ youcreep/page_parser/selectors/common.py
@@ -1,8 +1,21 @@
 """Selectors for components in common pages"""
 
+video_card_sel = "ytd-video-renderer"
+"""video card"""
+
+comment_card_sel = "ytd-comment-renderer"
+"""comment card for all comment, including head comment and replies"""
+
+head_comment_card_sel = "ytd-comment-thread-renderer"
+"""comment card for head comment"""
+
 search_input_sel = "input#search"
 """search input"""
+
 clear_input_btn_selector = "#search-clear-button > ytd-button-renderer > yt-button-shape > button"
 """clear search input button"""
+
 search_submit_sel = "#search-icon-legacy"
 """search submit button"""
+
+dismiss_premium_btn = "#dismiss-button > yt-button-renderer > yt-button-shape > button"
