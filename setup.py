1b2faa5453da5acd0465fbabe9be78671843c11f
stevieflyer <steveflyer7@gmail.com>
2023-08-24 13:50:41 +0800

    update version to 0.1.21

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements():
 
 setup(
     name='youcreep',
-    version='0.1.19',
+    version='0.1.21',
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=read_requirements(),
     url='https://github.com/stevieflyer/youtube_crawler',
